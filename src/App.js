0c790bac1d642f8a3667b3d38e0d961662ec3621
ToolReaz <toolreaz@outlook.fr>
2021-07-14 16:00:17 +0200

    Fix access of electron api in web components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+const { dialog } = window.require("electron").remote;
 
 function App() {
+  const test = () => {
+    dialog.showMessageBoxSync({ type: "info", message: "Hello from Yaal !" });
+  };
+
   return (
     <div className="App">
       <h1>Yaal</h1>
+      <button onClick={test}>Dialog test</button>
     </div>
   );
 }
